f0d11023ff5150e7f155efa2f2b37bb17631662c
daltonfontes <fontes.dalton@hotmail.com>
2024-08-05 21:07:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import moment from "moment-timezone";
+import chalk from "chalk";
+
+const URL = "https://www.ticketmaster.com.br/static/rock-in-rio";
+const TIME_VERIFY = 30 * 60 * 1000;
+const TIME_BOX = moment.tz('2024-08-06 12:00', 'America/Sao_Paulo');
+
+// Estilos de texto
+const info = chalk.blue;
+const success = chalk.green;
+const warning = chalk.yellow;
+const error = chalk.red;
+
+async function verificarDisponibilidade() {
+  try {
+    const resposta = await axios.get(URL);
+    if (resposta.status === 200) {
+      if (resposta.data.includes("Ingressos disponíveis")) {
+        return true;
+      }
+    }
+    return false;
+  } catch (erro) {
+    console.error(error(`Erro ao acessar o site: ${erro.message}`));
+    return false;
+  }
+}
+
+async function main() {
+  console.log(info("Iniciando verificação de disponibilidade de ingressos..."));
+
+  const hour = moment().tz('America/Sao_Paulo').format('HH:mm:ss');
+
+  while (moment().tz('America/Sao_Paulo').isBefore(TIME_BOX)) {
+    if (await verificarDisponibilidade()) {
+      console.log(success("Ingressos disponíveis! Acesse o site para comprar: ") + chalk.underline.blue(URL));
+      break;
+    } else {
+      console.log(warning(`[${hour}] Ingressos não disponíveis. Próxima verificação em ${TIME_VERIFY / 1000 / 60} minutos.`));
+    }
+    await new Promise(resolve => setTimeout(resolve, TIME_VERIFY));
+  }
+
+  if (moment().tz('America/Sao_Paulo').isSameOrAfter(TIME_BOX)) {
+    console.log(info("Tempo limite atingido. O script foi encerrado."));
+  }
+}
+
+main().catch(erro => console.error(error(`Erro no script: ${erro.message}`)));
